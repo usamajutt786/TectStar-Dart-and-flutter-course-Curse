499d5eaf99f2a96a5b342a0e278f7ae5162f2243
Usama <2021se41@student.uet.edu.pk>
2023-10-16 21:13:22 +0500

    Lecture15

---
+++ Lecture15.dart
@@ -0,0 +1,26 @@
+
+
+
+// funtion to add two numbers 
+int sum(int a,int b) {
+
+ // print("The sum of two numbers is:");
+    return a+b;
+}
+
+void menu()
+{
+  print("Hello please subscribe");
+  print("Hello please subscribe");
+  print("Hello please subscribe");
+  print("Hello please subscribe");
+  print("Hello please subscribe");
+}
+
+
+void main()
+{
+   int result=sum(12, 23);//= 12+23
+   print(result);
+   menu();
+}
